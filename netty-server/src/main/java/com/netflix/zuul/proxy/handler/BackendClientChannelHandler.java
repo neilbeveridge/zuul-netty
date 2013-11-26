2c8db5c67ff184441d285cea1366801ce26dbb9a
Kyle Vosper <kvosper@expedia.com>
2013-11-26 15:51:18 +0000

    Added missing channelReadComplete method Added debug logging Removed redundant listener - no need to repeat reads as all requests are aggregated

--- netty-server/src/main/java/com/netflix/zuul/proxy/handler/BackendClientChannelHandler.java
+++ netty-server/src/main/java/com/netflix/zuul/proxy/handler/BackendClientChannelHandler.java
@@ -2,8 +2,6 @@ package com.netflix.zuul.proxy.handler;
 
 import static org.slf4j.LoggerFactory.getLogger;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
@@ -25,14 +23,20 @@ public class BackendClientChannelHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelActive(ChannelHandlerContext context) throws Exception {
         context.read();
+
+        LOG.debug("channelActive");
     }
 
     @Override
     public void channelRead(final ChannelHandlerContext context, Object message) throws Exception {
+        LOG.debug("channelRead");
 
-        ChannelFuture future = inboundChannel.writeAndFlush(message);
+        inboundChannel.writeAndFlush(message);
+    }
 
-        future.addListener(new RepeatRead(context));
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        HandlerUtil.closeOnFlush(inboundChannel);
     }
 
     @Override
@@ -44,22 +48,5 @@ public class BackendClientChannelHandler extends ChannelInboundHandlerAdapter {
     public void exceptionCaught(ChannelHandlerContext context, Throwable cause) throws Exception {
         LOG.info("exception caught : ", cause);
         HandlerUtil.closeOnFlush(context.channel());
-    }
-
-    private final class RepeatRead implements ChannelFutureListener {
-        private final ChannelHandlerContext context;
-
-        private RepeatRead(ChannelHandlerContext context) {
-            this.context = context;
-        }
-
-        @Override
-        public void operationComplete(ChannelFuture future) throws Exception {
-            if (future.isSuccess()) {
-                context.channel().read();
-            } else {
-                future.channel().close();
-            }
-        }
     }
 }
